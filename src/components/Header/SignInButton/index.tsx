6ccb348876ad4c089ae69b42e10d193847a5f854
Lucas Martins Vasconcelos <lucas@Lucass-MacBook-Pro.local>
2022-03-26 17:28:25 -0300

    add authentication

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -2,18 +2,21 @@ import { useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
+import { signIn, useSession, signOut } from "next-auth/react"
 
 export function SignInbutton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession()
 
-  return isUserLoggedIn ? (
-    <button className={styles.SignInButton} type="button">
+  console.log(session)
+
+  return session ? (
+    <button onClick={() => signOut()} className={styles.SignInButton} type="button">
       <FaGithub color="#04d361" />
-      Lucas Martins
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : ( 
-    <button className={styles.SignInButton} type="button">
+    <button onClick={() => signIn('github')} className={styles.SignInButton} type="button">
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,32 @@
+import { query as q } from "faunadb";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { FaUndoAlt } from "react-icons/fa";
+
+import { fauna } from "../../../services/fauna";
+
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      //   scope: 'read:user'
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+            q.Create(
+                q.Collection("users"), { data: { email } }
+            )
+        );
+        return true;
+      } catch (error) {
+          return false
+      }
+    },
+  },
+});
